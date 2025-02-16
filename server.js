92389dcb990320fdea1c17086df9f26b12f54871
eshitarai27 <166654748+eshitarai27@users.noreply.github.com>
2025-02-16 23:37:20 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("./db");
+const routes = require("./routes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", routes);
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
